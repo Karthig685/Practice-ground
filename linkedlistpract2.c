2b418458035f16fd9fc836a1c88357d62c9ebcd4
Karthigeyan <karthigeyanpers@gmail.com>
2025-11-28 12:32:06 +0530

    Random Practice

--- linkedlistpract2.c
+++ linkedlistpract2.c
@@ -7,49 +7,42 @@ struct Node{
 };
 
 struct Node* createNode(int value){
-    struct Node *newNode=malloc(sizeof(struct Node));
+    struct Node* newNode=malloc(sizeof(struct Node));
     newNode->data=value;
     newNode->next=NULL;
     return newNode;
 }
 
-struct Node* beg(struct Node *head, int value){
-    struct Node *newNode=createNode(value);
+struct Node* bn(struct Node* head, int value){
+    struct Node* newNode=createNode(value);
     newNode->next=head;
     return newNode;
 }
 
-struct Node* end(struct Node *head, int value){
-    struct Node *newNode=createNode(value);
-    if(head==NULL) return newNode;
-    struct Node *temp=head;
-    while(temp->next!=NULL){
+struct Node* en(struct Node* head, int value){
+    struct Node* newNode=createNode(value);
+    if (head==NULL) return newNode;
+    struct Node* temp=head;
+    while (temp->next!=NULL){
         temp=temp->next;
     }
     temp->next=newNode;
     return head;
 }
 
-void printlist(struct Node *head){
-    struct Node *temp=head;
-    while(temp!=NULL){
+void pn(struct Node* head){
+    struct Node* temp=head;
+    while (temp!=NULL){
         printf("%d->", temp->data);
         temp=temp->next;
     }
     printf("NULL\n");
 }
-void printreverse(struct Node *head){
-    if (head==NULL) return;
-    printreverse(head->next);
-    printf("->%d", head->data);
-}
 
 int main(){
-    struct Node *head=NULL;
-    head=beg(head,10);
-    head=end(head,30);
-    head=end(head,50);
-    printlist(head);
-    printreverse(head);
-    return 0;
+    struct Node* head=NULL;
+    head=bn(head,10);
+    head=en(head,11);
+    head=en(head,12);
+    pn(head);
 }
